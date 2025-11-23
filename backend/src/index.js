a9bb4e7ea97b6e9d3328dc177df1bde9c24f1e1b
Shri Krishna R Bhat <shrikrishnarbhat2003@gmail.com>
2025-11-23 13:52:17 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,21 +4,21 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const employeesRoutes = require('./routes/employees');
 const teamsRoutes = require('./routes/teams');
-const analyticsRoutes = require('./routes/analytics'); // ADD THIS
+const analyticsRoutes = require('./routes/analytics'); 
 const { logOperation } = require('./utils/logger');
-const loggerMiddleware = require('./middleware/loggerMiddleware'); // ADD THIS
+const loggerMiddleware = require('./middleware/loggerMiddleware'); 
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(loggerMiddleware); // ADD THIS
+app.use(loggerMiddleware); 
 
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeesRoutes);
 app.use('/api/teams', teamsRoutes);
-app.use('/api/analytics', analyticsRoutes); // ADD THIS
+app.use('/api/analytics', analyticsRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
