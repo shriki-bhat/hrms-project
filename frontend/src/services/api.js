f3bfe754d07a4426e074811b568f2211d8f822a2
Shri Krishna R Bhat <shrikrishnarbhat2003@gmail.com>
2025-11-24 10:43:23 +0530

    API updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,35 +15,35 @@ login: (data) => axios.post(`${API_URL}/api/auth/login`, data),
 
 
     // Employees
-    getEmployees: () => axios.get(`${API_URL}/employees`, getAuthHeader()),
-    createEmployee: (data) => axios.post(`${API_URL}/employees`, data, getAuthHeader()),
-    updateEmployee: (id, data) => axios.put(`${API_URL}/employees/${id}`, data, getAuthHeader()),
-    deleteEmployee: (id) => axios.delete(`${API_URL}/employees/${id}`, getAuthHeader()),
+    getEmployees: () => axios.get(`${API_URL}/api/employees`, getAuthHeader()),
+    createEmployee: (data) => axios.post(`${API_URL}/api/employees`, data, getAuthHeader()),
+    updateEmployee: (id, data) => axios.put(`${API_URL}/api/employees/${id}`, data, getAuthHeader()),
+    deleteEmployee: (id) => axios.delete(`${API_URL}/api/employees/${id}`, getAuthHeader()),
 
     // Teams
-    getTeams: () => axios.get(`${API_URL}/teams`, getAuthHeader()),
-    getTeam: (id) => axios.get(`${API_URL}/teams/${id}`, getAuthHeader()),
-    createTeam: (data) => axios.post(`${API_URL}/teams`, data, getAuthHeader()),
-    updateTeam: (id, data) => axios.put(`${API_URL}/teams/${id}`, data, getAuthHeader()),
-    deleteTeam: (id) => axios.delete(`${API_URL}/teams/${id}`, getAuthHeader()),
+    getTeams: () => axios.get(`${API_URL}/api/teams`, getAuthHeader()),
+    getTeam: (id) => axios.get(`${API_URL}/api/teams/${id}`, getAuthHeader()),
+    createTeam: (data) => axios.post(`${API_URL}/api/teams`, data, getAuthHeader()),
+    updateTeam: (id, data) => axios.put(`${API_URL}/api/teams/${id}`, data, getAuthHeader()),
+    deleteTeam: (id) => axios.delete(`${API_URL}/api/teams/${id}`, getAuthHeader()),
 
     // Assign employee to team
     assignEmployee: (teamId, employeeId) =>
         axios.post(
-            `${API_URL}/teams/${teamId}/assign`,
+            `${API_URL}/api/teams/${teamId}/assign`,
             { employee_id: employeeId },
             getAuthHeader()
         ),
 
     unassignEmployee: (teamId, employeeId) =>
         axios.post(
-            `${API_URL}/teams/${teamId}/unassign`,
+            `${API_URL}/api/teams/${teamId}/unassign`,
             { employee_id: employeeId },
             getAuthHeader()
         ),
 
     // Analytics
-    getAnalytics: () => axios.get(`${API_URL}/analytics`, getAuthHeader()),
+    getAnalytics: () => axios.get(`${API_URL}/api/analytics`, getAuthHeader()),
 };
 
 export default api;
